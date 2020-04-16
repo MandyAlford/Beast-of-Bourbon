23b1dd1304d94b098eb15538bb27508d457ea024
Mandy Alford <mandyalford@gmail.com>
2020-04-15 20:27:59 -0600

    Initial header spacing, flex

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,26 @@
 import React from "react";
 import "./Header.css";
+import bourbon from "../../assets/bourbon.svg";
 
 const Header = () => {
   return (
-    <p>I am a Header</p>
+    <div className='header'>
+      <div className='logo-nav'>
+        <img className='nav-bourbon' src={bourbon} alt='bourbon-logo' />
+        <h1 className='nav-title'>Beast of Bourbon</h1>
+      </div>
+      <div className='greeting'>
+        <h2>Welcome, Bourbon Lover!</h2>
+      </div>
+      <div>
+          <button
+            aria-label = 'Favorites'
+            className = 'nav-button'
+            >
+            View Favorites
+          </button>
+      </div>    
+    </div>
   )
 }
 
