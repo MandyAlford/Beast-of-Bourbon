247cc2938ee53e07ccf4ab6fce5193fd20222f94
Mandy Alford <mandyalford@gmail.com>
2020-04-16 18:56:11 -0600

    Mock fetch to test implementation of redux

--- src/components/CardContainer/CardContainer.js
+++ src/components/CardContainer/CardContainer.js
@@ -1,14 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./CardContainer.css";
 import bartender from "../../assets/bartender.jpg";
 import DrinkCard from "../DrinkCard/DrinkCard";
+import { getDrinks } from '../../Actions';
+import { connect } from 'react-redux';
 
-const CardContainer = () => {
-  return(
-    <div className='card-container'>
-      <DrinkCard />
-    </div>
-  )
+class CardContainer extends Component {
+  constructor() {
+    super();
+  }
+
+  componentDidMount() {
+    const fetchResults = {
+      "drinks": [
+        {
+          "strDrink": "Allegheny",
+          "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/uwvyts1483387934.jpg",
+          "idDrink": "11021"
+        },
+        {
+          "strDrink": "Bourbon Sour",
+          "strDrinkThumb": "https://www.thecocktaildb.com/images/media/drink/dms3io1504366318.jpg",
+          "idDrink": "11147"
+        }
+      ]
+    }
+
+    this.props.setDrinks(fetchResults.drinks)
+
+  }
+
+  render() {
+    return(
+      <div className='card-container'>
+        <DrinkCard />
+      </div>
+    )
+  }
 }
 
-export default CardContainer;
+const mapDispatchToProps = dispatch => ({
+  setDrinks: data => dispatch( getDrinks(data) )
+})
+
+export default connect(null, mapDispatchToProps)(CardContainer);
