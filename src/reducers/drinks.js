d72254c6d5ca74cf862aa9796ca15f1aa141d0b9
Mandy Alford <mandyalford@gmail.com>
2020-04-16 20:50:33 -0600

    Update return on drink reducer

--- src/reducers/drinks.js
+++ src/reducers/drinks.js
@@ -1,7 +1,7 @@
 export const drinks = (state = [], action) => {
   switch (action.type) {
     case 'GET_DRINKS':
-      return action
+      return [...state, ...action.drinks]
     default:
       return state;
   }
