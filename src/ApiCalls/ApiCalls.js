31825aada37697ed9e65ed23c07265a3be1b861d
Mandy Alford <mandyalford@gmail.com>
2020-04-16 20:46:17 -0600

    Update fetch call

--- src/ApiCalls/ApiCalls.js
+++ src/ApiCalls/ApiCalls.js
@@ -1,5 +1,5 @@
 
 export const fetchDrinks = () => {
-  return fecth(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon`)
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon`)
     .then((response) => response.json())
 }
