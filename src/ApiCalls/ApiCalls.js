883d6add10e486bf2fdc2387418afb3f063f5922
Mandy Alford <mandyalford@gmail.com>
2020-04-18 21:22:14 -0600

    Write fetch call to get one detailed  drink recipe

--- src/ApiCalls/ApiCalls.js
+++ src/ApiCalls/ApiCalls.js
@@ -3,3 +3,8 @@ export const fetchDrinks = () => {
   return fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=Bourbon`)
     .then((response) => response.json())
 }
+
+export const fetchDrinkRecipe = (id) => {
+  return fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${id}`)
+    .then((response) => response.json())
+}
