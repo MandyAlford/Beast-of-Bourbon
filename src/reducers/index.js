ec89d42ef1eb9fbe3d75562d910b4abdf2137e2a
Mandy Alford <mandyalford@gmail.com>
2020-04-18 21:25:05 -0600

    Add recipe to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { drinks } from './drinks';
+import { recipe } from './recipe';
 
 const rootReducer = combineReducers({
   drinks,
+  recipe
 })
 
 export default rootReducer;
