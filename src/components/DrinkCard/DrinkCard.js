2930ef2cf7530f5bf900755ff02a772a9f231873
Mandy Alford <mandyalford@gmail.com>
2020-04-18 21:24:36 -0600

    Make DrinkCard a class component in order to write and use getDrinkDetails method and use props via state

--- src/components/DrinkCard/DrinkCard.js
+++ src/components/DrinkCard/DrinkCard.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 import "./DrinkCard.css";
+import { fetchDrinkRecipe } from '../../ApiCalls/ApiCalls';
+import { getDrinkRecipe } from '../../Actions';
+import { connect } from 'react-redux';
 
-const DrinkCard = (props) => {
+class DrinkCard extends Component {
+  constructor(){
+    super();
+  }
+
+getDrinkDetails = (event) => {
+  let drinkId = event.target.parentElement.id
+  fetchDrinkRecipe(drinkId).then((data) => {
+    this.props.setCurrentRecipe(data.drinks[0])
+  })
+  // take the id from parent element,
+  // interpolate the id into fetch request and fetch data
+  // set recipe in store
+  // go to new route path
+  // display drink recipe pn page
+
+}
+
+render() {
   return (
-    <div className='drink-card'>
-      <h1 className='drink-card-header'>{props.name}</h1>
-      <img className='drink-card-image' src={props.image} alt={`${props.name}-image`}
+    <div className='drink-card' id={this.props.id} onClick={this.getDrinkDetails}>
+      <h1 className='drink-card-header'>{this.props.name}</h1>
+      <img className='drink-card-image' src={this.props.image} alt={`${this.props.name}-image`}
       />
     </div>
   )
 }
+}
 
-export default DrinkCard;
+const mapDispatchToProps = dispatch => ({
+  setCurrentRecipe: data => dispatch( getDrinkRecipe(data) )
+})
+
+export default connect(null, mapDispatchToProps)(DrinkCard);
