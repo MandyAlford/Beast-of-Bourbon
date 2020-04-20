2b8cc18aae91303df4d15b5628098fe7ba6918f2
Mandy Alford <mandyalford@gmail.com>
2020-04-20 12:40:55 -0600

    Add conditional to favorites reducer to prevent duplicates

--- src/reducers/favorites.js
+++ src/reducers/favorites.js
@@ -1,7 +1,9 @@
 export const favorites = (state = [], action) => {
   switch (action.type) {
     case 'ADD_FAVORITE':
+      if(!state.includes(action.id)){
       return [...state, action.id]
+    }
     default:
     return state;
   }
