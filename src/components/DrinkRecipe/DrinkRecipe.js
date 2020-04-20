3d6a92f18924597041056b95e8b211a2b63d81cb
Mandy Alford <mandyalford@gmail.com>
2020-04-20 11:09:46 -0600

    Change DrinkRecipe from functional to class Component

--- src/components/DrinkRecipe/DrinkRecipe.js
+++ src/components/DrinkRecipe/DrinkRecipe.js
@@ -1,36 +1,42 @@
-import React from "react";
+import React, { Component } from "react";
 import "./DrinkRecipe.css";
 import { connect } from "react-redux";
 import RecipeConverter from '../../util/RecipeConverter';
 import rootReducer from '../../reducers'
 import { addDrinkToFavorites } from '../../Actions';
 
-const DrinkRecipe = (props) => {
-  if(Object.keys(props.recipe).length > 0) {
-    let recipeConverter = new RecipeConverter(props.recipe)
-    let recipeIngredients = recipeConverter.orangizeIngredients()
+class DrinkRecipe extends Component {
+  constructor() {
+    super();
+  }
 
-    let ingredientElements = recipeIngredients.map(ingredient => {
-      return <li>{ingredient}</li>
-    })
-    return(
-      <div className='drink-recipe' id={props.recipe.idDrink}>
-        <div className='recipe-details'>
-          <h2>{props.recipe.strDrink}</h2>
-          <p>Type: {props.recipe.strCategory}</p>
-          <p>Glassware: {props.recipe.strGlass}</p>
-          <p>Ingredients:</p>
-          <ul> { ingredientElements } </ul>
-          <p>{props.recipe.strInstructions}</p>
-          <button className='add-favorite-button'>Add Favorite</button>
+  render() {
+    if(Object.keys(this.props.recipe).length > 0) {
+      let recipeConverter = new RecipeConverter(this.props.recipe)
+      let recipeIngredients = recipeConverter.orangizeIngredients()
+
+      let ingredientElements = recipeIngredients.map(ingredient => {
+        return <li>{ingredient}</li>
+      })
+      return(
+        <div className='drink-recipe' id={this.props.recipe.idDrink}>
+          <div className='recipe-details'>
+            <h2>{this.props.recipe.strDrink}</h2>
+            <p>Type: {this.props.recipe.strCategory}</p>
+            <p>Glassware: {this.props.recipe.strGlass}</p>
+            <p>Ingredients:</p>
+            <ul> { ingredientElements } </ul>
+            <p>{this.props.recipe.strInstructions}</p>
+            <button className='add-favorite-button'>Add Favorite</button>
+          </div>
+          <div>
+            <img className='drink-recipe-image' src={this.props.recipe.strDrinkThumb} alt={`${this.props.recipe.strDrink}-image`}/>
+          </div>
         </div>
-        <div>
-          <img className='drink-recipe-image' src={props.recipe.strDrinkThumb} alt={`${props.recipe.strDrink}-image`}/>
-        </div>
-      </div>
-    )
-  } else {
-    return ''
+      )
+    } else {
+      return ''
+    }
   }
 }
 
