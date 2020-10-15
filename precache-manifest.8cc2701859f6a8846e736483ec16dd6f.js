f644a32537a6254f4e59e1769d3968db60fb1738
Mandy Alford <mandyalford@gmail.com>
2020-10-15 12:20:06 -0400

    Updates

---
+++ precache-manifest.8cc2701859f6a8846e736483ec16dd6f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a79f2585818c6890b73f751ac12c5b66",
+    "url": "/Beast-of-Bourbon/index.html"
+  },
+  {
+    "revision": "f5fe08249aee4ffdf44c",
+    "url": "/Beast-of-Bourbon/static/css/main.ea8a1bc0.chunk.css"
+  },
+  {
+    "revision": "6e3892e41bc79fc879aa",
+    "url": "/Beast-of-Bourbon/static/js/2.3dd78fce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Beast-of-Bourbon/static/js/2.3dd78fce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5fe08249aee4ffdf44c",
+    "url": "/Beast-of-Bourbon/static/js/main.815c0317.chunk.js"
+  },
+  {
+    "revision": "1f3793eaa87f0b70fa32",
+    "url": "/Beast-of-Bourbon/static/js/runtime-main.c37337df.js"
+  },
+  {
+    "revision": "d87548432992455176108beb7c5997b2",
+    "url": "/Beast-of-Bourbon/static/media/bartender.d8754843.jpg"
+  },
+  {
+    "revision": "e5ec81b5ed74ea791b3e32da3e539b9a",
+    "url": "/Beast-of-Bourbon/static/media/bourbon.e5ec81b5.svg"
+  }
+]);
